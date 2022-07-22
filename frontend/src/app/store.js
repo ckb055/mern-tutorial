e37807f672e230bbc05ce631fe35ed825983b46a
Chen Kai Bin <chenkaibin7@gmail.com>
2022-07-22 12:45:39 +0800

    Update store.js
    Desc

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import goalReducer from '../features/goals/goalSlice'
-
+//test
 export const store = configureStore({
   reducer: {
     auth: authReducer,
